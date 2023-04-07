7cfaf41fae2bfaeffb51980d8c1b98f98ce97118
milindkumar64 <milindgupta@gmail.com>
2023-04-07 17:26:06 +0530

    Add: Peak and pop the elements till the Stack is empty

--- src/com/bridgelabz/StackAndQueueProblem.java
+++ src/com/bridgelabz/StackAndQueueProblem.java
@@ -3,40 +3,51 @@ package com.bridgelabz;
 import java.util.LinkedList;
 
 class Stack {
-	   private LinkedList linkedlist = new LinkedList();
-	   public void push(Object obj) {
-		  
-		   linkedlist.addFirst(obj);
-	   }
-	   
-	   public Object top() {
-	      return linkedlist.getFirst();
-	   }
-	   public Object pop() {
-	      return linkedlist.removeFirst();
-	   }
-	   public String toString()
-	   {
-		return ""+linkedlist;
-		   
-	   }
+	private LinkedList linkedlist = new LinkedList();
+
+	public void push(Object obj) {
+
+		linkedlist.addFirst(obj);
 	}
 
+	public Object peak() {
+		return linkedlist.getFirst();
+	}
+
+	public Object pop() {
+		return linkedlist.removeFirst();
+	}
+
+	public boolean isEmpty() {
+		if (linkedlist.isEmpty()) {
+			return true;
+		}
+		return false;
+	}
+
+	public String toString() {
+		return "" + linkedlist;
+
+	}
+}
+
 public class StackAndQueueProblem {
-	
-	
 
 	public static void main(String[] args) {
-		
-		//StackAndQueueProblem stackAndQueueProblem = new StackAndQueueProblem();
- 		Stack stack = new Stack();
- 		
- 		stack.push(70);
- 		stack.push(30);
- 		stack.push(56);
- 		
- 		System.out.println(stack);
-		
+
+		Stack stack = new Stack();
+
+		stack.push(70);
+		stack.push(30);
+		stack.push(56);
+
+		System.out.println(stack);
+
+		while (!stack.isEmpty()) {
+			System.out.println("Now Peak is at :"+stack.peak());
+			System.out.println(stack.pop()+" is removed ");
+		}
+		System.out.println("Stack is empty : "+stack);
 
 	}
 
