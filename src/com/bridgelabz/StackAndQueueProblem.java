808a40d961d2f719277b4e2c63d6a559e77b8dcd
milindkumar64 <milindgupta@gmail.com>
2023-04-07 18:56:01 +0530

    Add: Dequeue the values from Queue

--- src/com/bridgelabz/StackAndQueueProblem.java
+++ src/com/bridgelabz/StackAndQueueProblem.java
@@ -10,11 +10,21 @@ class Queue {
 		linkedlist.addLast(obj);
 	}
 
+	public Object dequeue()
+	{	
+		return linkedlist.remove();
+	}
+	
+	public boolean isEmpty()
+	{
+		return linkedlist.isEmpty();
+	}
 	public String toString() {
 		return "" + linkedlist;
 
 	}
 }
+
 public class StackAndQueueProblem {
 
 	public static void main(String[] args) {
@@ -26,6 +36,12 @@ public class StackAndQueueProblem {
 		queue.enqueu(70);
 
 		System.out.println(queue);
-
+		
+		while(!queue.isEmpty())
+		{
+			queue.dequeue();
+		}
+		System.out.println("Queue is empty : "+queue);
+ 
 	}
 }
